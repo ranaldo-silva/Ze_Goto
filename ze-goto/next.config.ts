396ecc9be38f75ca0ba42a8f32dcbbf762b0e574
EquipeFiapProjeto <rm559210@fiap.com.br>
2025-06-06 14:07:41 -0300

    atualização next config

--- ze-goto/next.config.ts
+++ ze-goto/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
-
+ 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ⚠️ ignora erros de TS — use com cautela
+  },
 };
-
+ 
 export default nextConfig;
